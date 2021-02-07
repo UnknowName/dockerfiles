325d2d417490fa23f0ecff0a665401142956ad77
UnknowName <burtte@sina.com>
2021-02-07 15:06:47 +0800

    update

--- freeswitch/docker-entrypoint.sh
+++ freeswitch/docker-entrypoint.sh
@@ -4,50 +4,41 @@ set -e
 
 if [ $1 = "freeswitch" ];then
     if [ -z ${MYSQL_ADDR} ];then
-        echo "not set MYSQL_ADDR, will use default file to stage FreeSWITCH core"
+        echo "not set MYSQL_ADDR env, will use default file to stage FreeSWITCH core"
     else
-        if [ ${MYSQL_DATABASE} ]; then
-            if [ -z ${MYSQL_USER} ];then
-                echo "Use default root for mysql user"
-                MYSQL_USER="root"
-            fi
-            if [ -z ${MYSQL_PASSWORD} ];then
-                echo "MySQL user ${MYSQL_USER} password not set!"
-                exit 1
-            fi
-            # Create ODBC config file
-        else
+        if [ -z ${MYSQL_DATABASE} ];then
+            echo "no set MYSQL_DATABSE env, use default name freeswitch"
             MYSQL_DATABASE="freeswitch"
         fi
-        echo "set MYSQL_ADDR, check user and passsword"
+        if [ -z ${MYSQL_PORT} ];then
+            echo "no set MYSQL_PORT env,use default 3306"
+            MYSQL_PORT="3306"
+        fi
+        if [ -z ${MYSQL_USER} ];then
+            echo "no set MYSQL_USER env, use default root"
+            MYSQL_USER="root"
+        fi
+        if [ -z ${MYSQL_PASSWORD} ];then
+            echo "no set MYSQL_PASSWORD env, exit!"
+            exit 1
+        fi
+
+        echo "[mysql]" > /etc/odbcinst.ini
+        echo "Description=mysql" >> /etc/odbcinst.ini
+        echo "Driver=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc.so" >> /etc/odbcinst.ini
+        echo "Setup=/usr/lib/x86_64-linux-gnu/odbc/libodbcmyS.so" >> /etc/odbcinst.ini
+        echo "FileUsage=1" >> /etc/odbcinst.ini
+
+        echo "[freeswitch]" > /etc/odbc.ini
+        echo "Description=Mysql ODBC 2.3 DSN" >> /etc/odbc.ini
+        echo "erver=${MYSQL_ADDR}" >> /etc/odbc.ini
+        echo "Port=${MYSQL_PORT}" >> /etc/odbc.ini
+        echo "User=${MYSQL_USER}" >> /etc/odbc.ini
+        echo "Password=${MYSQL_PASSWORD}" >> /etc/odbc.ini
+        echo "Database=${MYSQL_DATABASE}l" >> /etc/odbc.ini
+        echo "Option=3" >> /etc/odbc.ini
     fi
     /usr/local/freeswitch/bin/freeswitch
 else
     exec "$@"
-fi
-
-# Create /etc/odbcini.conf
-if [ ! -f "/etc/odbcinst.ini" ];then
-cat > /etc/odbcinst.ini <<  EOF
-[mysql]
-Description=mysql
-Driver=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc.so
-Setup=/usr/lib/x86_64-linux-gnu/odbc/libodbcmyS.so
-FileUsage=1
-EOF
-fi
-
-if [ ! -f "/etc/odbc.ini" ];then
-cat > /etc/odbc.ini << EOF
-[freeswitch]
-Driver=mysql
-Description=Mysql ODBC 2.3 DSN
-Server=${MYSQL_ADDR}
-Port=${MYSQL_PORT}
-User=${MYSQL_USER}
-Password=${MYSQL_PASSWORD}
-Database=${MYSQL_DATABASE}
-Option=3
-Socket=
-EOF
 fi
