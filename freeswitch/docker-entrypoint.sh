ebabd8ad7579893105e2228b53b4a38dec3d0d84
UnknowName <burtte@sina.com>
2021-02-08 10:31:33 +0800

    update

--- freeswitch/docker-entrypoint.sh
+++ freeswitch/docker-entrypoint.sh
@@ -23,6 +23,7 @@ if [ $1 = "freeswitch" ];then
             exit 1
         fi
 
+        # Use ODBC MySQL
         echo "[mysql]" > /etc/odbcinst.ini
         echo "Description=mysql" >> /etc/odbcinst.ini
         echo "Driver=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc.so" >> /etc/odbcinst.ini
@@ -30,15 +31,18 @@ if [ $1 = "freeswitch" ];then
         echo "FileUsage=1" >> /etc/odbcinst.ini
 
         echo "[freeswitch]" > /etc/odbc.ini
-        echo "Description=Mysql ODBC 2.3 DSN" >> /etc/odbc.ini
-        echo "erver=${MYSQL_ADDR}" >> /etc/odbc.ini
+        echo "Driver=mysql" >> /etc/odbc.ini
+        echo "Description=MySQL ODBC DSN" >> /etc/odbc.ini
+        echo "Server=${MYSQL_ADDR}" >> /etc/odbc.ini
         echo "Port=${MYSQL_PORT}" >> /etc/odbc.ini
         echo "User=${MYSQL_USER}" >> /etc/odbc.ini
         echo "Password=${MYSQL_PASSWORD}" >> /etc/odbc.ini
-        echo "Database=${MYSQL_DATABASE}l" >> /etc/odbc.ini
+        echo "Database=${MYSQL_DATABASE}" >> /etc/odbc.ini
         echo "Option=3" >> /etc/odbc.ini
+        sed -i 's/<!--<param name="odbc-dsn" value="dsn:user:pass"\/>-->/<param name="odbc-dsn" value="freeswitch::"\/>/g' /etc/freeswitch/sip_profiles/internal.xml
     fi
-    /usr/local/freeswitch/bin/freeswitch
+    # /usr/local/freeswitch/bin/freeswitch
 else
-    exec "$@"
+    # exec "$@"
 fi
+exec "$@"
